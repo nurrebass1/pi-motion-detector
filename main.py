54ec68a02a940f2a5527df0e994aa64ffeecdae6
Nourhan Ragab <nourhan.ragab@acando.no>
2018-10-26 13:52:45 +0200

    Project files

---
+++ main.py
@@ -0,0 +1,20 @@
+def helloWorld():
+    print 'hello world!'
+
+
+helloWorld()
+
+
+# def detectMotion():
+#
+# def boot_led_on():
+#
+# def boot_led_off():
+#
+# def motion_led_on():
+#
+# def motion_led_off():
+#
+# def reset():
+
+
---
+++ pi_boot.py
@@ -0,0 +1,3 @@
+#boot and turn off pi on button click
+
+#run script on boot
